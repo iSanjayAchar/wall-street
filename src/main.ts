92051697152f6cd41260cbba658c55a63f36c5b9
Sanjay Achar <mr.sanjayachar@gmail.com>
2022-12-01 19:01:52 +0530

    feat: implementing sentry

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,42 @@
-import 'dotenv/config';
-import WebSocket from 'ws';
-import dayjs from 'dayjs';
-import { Config } from './config';
-import { Write } from './services/mongodb';
+import 'dotenv/config'
+import WebSocket from 'ws'
+import dayjs from 'dayjs'
+import { Config } from './config.js'
+import { Write } from './services/mongodb.js'
+import * as Sentry from '@sentry/node'
+
+/**
+ * Initializing sentry
+ */
+Sentry.init({
+  dsn: 'https://e517209df3c143f2af3c4ddea70188ed@o4504253951115264.ingest.sentry.io/4504253952622592',
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
 
 const socketConfig: WebSocket.ClientOptions = {
   perMessageDeflate: false,
-};
+}
 
-const socket = new WebSocket('wss://ws-plus.olymptrade.com/connect', socketConfig);
+const socket = new WebSocket('wss://ws-plus.olymptrade.com/connect', socketConfig)
 
 socket.on('open', function open() {
-  socket.send('[{"e":1000,"t":2,"d":{"pairs":["ASIA_X"]},"uuid":"2"}]');
-});
+  socket.send('[{"e":1000,"t":2,"d":{"pairs":["ASIA_X"]},"uuid":"2"}]')
+})
 
-socket.on('message', async (data) => {
-  const object = JSON.parse(data.toString());
-  if (!object || !object[0] || !object[0].d[0]) {
-    return;
+socket.on('message', async data => {
+  try {
+    const object = JSON.parse(data.toString())
+    if (!object || !object[0] || !object[0].d[0]) {
+      return
+    }
+
+    const { t, m } = object[0].d[0]
+    await Write(Config.pair.default, new Date(dayjs.unix(t).toISOString()), m)
+  } catch (err) {
+    Sentry.captureException(err)
   }
-
-  const { t, m } = object[0].d[0];
-  await Write(
-    Config.pair.default, 
-    new Date(dayjs.unix(t).toISOString()), 
-    m
-  );
-  console.log('Wrote to database');
-});
+})
--- src/services/mongodb.ts
+++ src/services/mongodb.ts
@@ -1,50 +1,49 @@
-import { MongoClient } from 'mongodb';
-import { Config } from '../config';
+import { MongoClient } from 'mongodb'
+import { Config } from '../config.js'
+import * as Sentry from '@sentry/node'
 
-const connection = new MongoClient(Config.database.mongodb.uri);
+const connection = new MongoClient(Config.database.mongodb.uri)
 
-await connection.connect();
+await connection.connect()
 
 /**
  * This function lists all collections in the database and checks if 'price-points' exists
  * If the collection doesn't exists, it will create one
  */
-const collections = (await connection.db(Config.database.mongodb.dbName).listCollections().toArray()).map((collection) => collection.name);
+const collections = (await connection.db(Config.database.mongodb.dbName).listCollections().toArray()).map(
+  collection => collection.name,
+)
 if (!collections.includes('price-points')) {
-    await Init();
+  await Init()
 }
 
 /**
  * Initializes the timeseries collection to be used later
  */
 async function Init() {
-    await connection.db(Config.database.mongodb.dbName).createCollection('price-points', {
-        timeseries: {
-            metaField: 'pair',
-            timeField: 'timestamp',
-            granularity: 'seconds',
-        },
-        expireAfterSeconds: 86400
+  await connection.db(Config.database.mongodb.dbName).createCollection('price-points', {
+    timeseries: {
+      metaField: 'pair',
+      timeField: 'timestamp',
+      granularity: 'seconds',
+    },
+    expireAfterSeconds: 86400,
+  })
+}
+
+async function Write(pair = Config.pair.default, timestamp = new Date(), price = 0) {
+  try {
+    await connection.db(Config.database.mongodb.dbName).collection('price-points').insertOne({
+      pair,
+      timestamp,
+      price,
     })
+
+    return Promise.resolve(true)
+  } catch (err) {
+    Sentry.captureException(err)
+    return Promise.reject(err)
+  }
 }
 
-async function Write(
-    pair = Config.pair.default,
-    timestamp = new Date(),
-    price = 0
-) {
-    try {
-        await connection.db(Config.database.mongodb.dbName).collection('price-points').insertOne({
-            pair, timestamp, price,
-        });
-
-        return Promise.resolve(true);
-    } catch (err) {
-        console.error(err);
-        return Promise.reject(err);
-    }
-}
-
-export {
-    Write,
-}
+export { Write }
